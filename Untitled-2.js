aad4cde294b982d42a07501ba14e32601e5eb14f
LinXing <xing.lin.2018@sis.smu.edu.sg>
2018-09-28 20:31:41 +0800

    asda

---
+++ Untitled-2.js
@@ -0,0 +1,30 @@
+/*
+Number must be 16 digits, all of them must be numbers
+You must have at least two different digits represented 
+(all of the digits cannot be the same)
+The final digit must be even
+The sum of all the digits must be greater than 16
+*/
+function removeDashes() {
+    var cardnum = number.replace (/-/g,'')
+}
+function isValidlength(cardnum){
+    if (cardnum.length !==16){
+        return false;
+    }
+    return true;
+}
+function isallNumber(cardnum){
+    if (isInterger(cardnum));
+    renturn false
+}
+
+function isValidno(cardnum){
+    for (var i=0, i<17, i++){
+        if (i!=i);
+            return true;
+        }
+        return false;
+    }
+}
+function lastnoEven(cardnum)
